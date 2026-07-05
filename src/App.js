454744b00e6a622405003c85a76a1bdfe981732a
FABIO VERLAINE <92590182+fabioverlaine@users.noreply.github.com>
2026-07-05 00:16:47 -0300

    primeiro commit projeto já iniciado com estrutura / header.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import RouterApp from "./routes";
+
+function App() {
+  return (
+    <div className="App">
+      <RouterApp />
+    </div>
+  );
+}
+export default App;
---
+++ src/pages/Filme/index.js
@@ -0,0 +1,8 @@
+function Filme() {
+    return(
+        <div>
+            <h1>Bemvindo a página detalhes do filme</h1>
+        </div>
+    );
+}
+export default Filme;
